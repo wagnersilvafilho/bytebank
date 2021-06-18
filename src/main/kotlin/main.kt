ab33f686f621a5808c7915e0a4ead77477b0448c
wagnersilvafilho <wagnersf97@gmail.com>
2021-06-18 09:51:40 -0300

    Criando classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,39 +1,72 @@
 fun main() {
-    println("Bem-vindo ao bytebank!")
-    var i = 0
-    while (i < 5){
-    //for (i in 5 downTo 1) {
-        //if (i == 4){
-        //   continue
-        //}
-        val titular: String = "Wagner $i"
-        val numeroConta: Int = 1000 + i
-        var saldo: Double = i + 10.0
-        //saldo = 100 + 2.0
-        //saldo += 200
-        //saldo = -1000.0
-        println("Titular: $titular")
-        println("Número da conta: $numeroConta")
-        println("Saldo da conta: $saldo")
-        println()
-        i++
+    println("Bem vindo ao Bytebank")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+//fun testaLacos(){
+//    var i = 0
+//    while(i < 5){
+//        val titular: String = "Alex $i"
+//        @@ -13,18 +39,16 @@ fun main() {
+//            i++
+//        }
+
+//    for (i in 5 downTo 1) {
+//        val titular: String = "Alex $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = i + 10.0
+//
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//    }
+
+//    testaCondicoes(saldo)
+//        for (i in 5 downTo 1) {
+//            val titular: String = "Alex $i"
+//            val numeroConta: Int = 1000 + i
+//            var saldo = i + 10.0
+//            println("titular $titular")
+//            println("número da conta $numeroConta")
+//            println("saldo da conta $saldo")
+//            println()
+//        }
+//    }
+
+    fun testaCondicoes(saldo: Double) {
+        if (saldo > 0.0) {
+            println("conta é positiva")
+        } else if (saldo == 0.0) {
+            println("conta é neutra")
+        } else {
+            println("conta é negativa")
         }
-    //testaCondicoes(saldo);
-}
-
-fun testaCondicoes(saldo: Double) {
-
-    if (saldo > 0.0) {
-        println("Conta é positiva")
-    } else if (saldo == 0.0) {
-        println("Conta é neutra")
-    } else {
-        println("Conta é negativa")
+        when {
+            saldo > 0.0 -> println("conta é positiva")
+            saldo == 0.0 -> println("conta é neutra")
+            else -> println("conta é negativa")
+        }
     }
-
-    when {
-        saldo > 0.0 -> println("Conta é positiva")
-        saldo == 0.0 -> println("Conta é neutra")
-        else -> println("Conta é negativa")
-    }
-}
